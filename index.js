b09ec035ae27aee5fd902b59c4d7e1ae0016e232
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-05-13 08:05:50 +0530

    views file path correction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import express from "express";
+import path from "path";
 import databaseConnection from "./database/connection.js";
 
 config();
@@ -7,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 databaseConnection();
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(`${path.dirname(import.meta.url)}/views`));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
