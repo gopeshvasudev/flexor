f4ff6128784c0edb9be153ef4e8d95744a81e91f
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-05-13 08:51:04 +0530

    again fixing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 databaseConnection();
 app.set("view engine", "ejs");
-app.set("views", path.join(path.dirname(import.meta.url), "views"));
+app.set("views", 'D:/projects/vs code projects/websites/flexor/views');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -18,7 +18,7 @@ app.use(cookieParser());
 import indexRouter from "./routes/index.js";
 import adminRouter from "./routes/admin.js";
 import cookieParser from "cookie-parser";
-app.use(indexRouter);
+app.use('/', indexRouter);
 app.use("/admin", adminRouter);
 
-app.listen(PORT, () => console.log("Example app listening on port 3000!"));
+app.listen(PORT, () => console.log(`express app listening on port: ${PORT}!`));
